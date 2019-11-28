1aba1732020c51cc36329deb254c584a3ec3c838
João Ricardo Cardoso <jricardorcardoso@gmail.com>
2019-11-28 20:13:36 +0000

    Improved web app appearance and fixed likes display

--- WebApplication/src/main.dart
+++ WebApplication/src/main.dart
@@ -1,35 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:web_app/SessionScreen.dart';
-//import 'QuestionsPage.dart';
-//import 'dart:async';
 
-int cnt = 0;
-
-void main() {
-  MyApp app = new MyApp();
-
-  /* Timer.periodic(Duration(seconds: 1), (timer) {
-    cnt++;
-    if (cnt > 1) {
-      timer.cancel();
-    } else app.updateQuestions();
-  });
-
-  Timer.periodic(Duration(seconds: 20), (timer) {
-    app.updateQuestions();
-  }); */
-
-  runApp(app);
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
-  //final QuestionsPage questionsPage = new QuestionsPage("0");
   final SessionScreen sessionScreen = new SessionScreen();
-
-  /* updateQuestions() {
-    questionsPage.updateQuestions();
-  } */
 
   @override
   Widget build(BuildContext context) {
