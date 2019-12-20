7275926a29412d088ec41943f9c546d54b22c50e
João Ricardo Cardoso <jricardorcardoso@gmail.com>
2019-12-20 10:04:08 +0000

    Deleted unnecessary files

--- mobile_app/build/app/intermediates/flutter/release/android-arm/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: C:\Users\sergi\OneDrive\Ambiente de Trabalho\flutterFolder\flutter\bin\cache\artifacts\engine\android-arm-release\windows-x64\gen_snapshot
--- mobile_app/build/app/intermediates/flutter/release/android-arm64/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: C:\Users\sergi\OneDrive\Ambiente de Trabalho\flutterFolder\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
